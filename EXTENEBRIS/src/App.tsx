e1338c1cf3cdb235f747e6166f2703ca7e9dde32
Aziel Meza <mezzaaazielle@gmail.com>
2025-11-04 18:53:17 -0500

    navegacion

--- EXTENEBRIS/src/App.tsx
+++ EXTENEBRIS/src/App.tsx
@@ -4,23 +4,39 @@ import Gallery from './assets/components/Galeria/Gallery';
 import Navbar from './assets/components/Navbar';
 import Team from './assets/components/Team';
 import Footer from './assets/components/Footer';
-import News from './assets/components/news'
+import News from './assets/components/news';
+
 const App: React.FC = () => {
   return (
     <main className="scroll-smooth bg-black text-white font-cinzel">
       
+      {/* Sección principal */}
       <section id="hero">
         <Hero />
       </section>
-       <Navbar />
 
-     
+      {/* Navbar fijo arriba */}
+      <Navbar />
+
+      {/* Galería (Inferno) */}
       <section id="inferno">
         <Gallery />
       </section>
-       <Team/>
-       <News/>
-       <Footer/>
+
+      {/* Equipo */}
+      <section id="team">
+        <Team />
+      </section>
+
+      {/* Noticias */}
+      <section id="news">
+        <News />
+      </section>
+
+      {/* Pie de página */}
+      <section id="footer">
+        <Footer />
+      </section>
     </main>
   );
 };
