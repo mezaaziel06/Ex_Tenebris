8e4195b1159923dd63b2bea65afd7fcb0d0a5bb7
Aziel Meza <mezzaaazielle@gmail.com>
2025-11-27 11:22:38 -0500

    apk

--- EXTENEBRIS/dev-dist/sw.js
+++ EXTENEBRIS/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.eduehjeqe5s"
+    "revision": "0.57lh12eues4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
